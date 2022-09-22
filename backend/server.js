ae01b0da5db32963266a8106f8967ff970449e98
Tushar Pethkar <pethkartusharnarendra@gmail.com>
2022-09-22 23:26:20 +0530

    ha

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import productRouter from './routes/productRoutes.js';
 import userRouter from './routes/userRoutes.js';
 import orderRouter from './routes/orderRoutes.js';
 import uploadRouter from './routes/uploadRoutes.js';
+import path from 'path';
 
 dotenv.config();
 
@@ -27,6 +28,12 @@ app.use('/api/products', productRouter);
 app.use('/api/users', userRouter);
 app.use('/api/order', orderRouter);
 app.use('/api/upload', uploadRouter);
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+);
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
