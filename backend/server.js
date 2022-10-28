7a2a77dca4f6e19ff17d54a1d6aabdcf6ebf9c34
Tushar Pethkar <pethkartusharnarendra@gmail.com>
2022-10-28 16:28:38 +0530

    final

--- backend/server.js
+++ backend/server.js
@@ -30,9 +30,9 @@ app.use('/api/upload', uploadRouter);
 
 const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, '/frontend/build')));
-// app.get('*', (req, res) =>
-//   res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
-// );
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+);
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
